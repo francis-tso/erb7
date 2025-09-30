620b2646a66a06a110f74ecaea655653d9667376
tim <count_francis@hotmail.com>
2025-09-30 17:47:55 +0800

    Step 9 : admin data models

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Listing
+from django.forms import NumberInput
+from django.db import models
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = 'id','title', 'is_published', 'doctor'
+    list_display_links = 'id', 'title'
+    list_filter = "doctor",
+    list_editable =("is_published"), 
+    search_fields = "title", "district", "doctor"
+    list_per_page = 25
+    ordering=['-id']
+    #prepopulated_fields = {"title" : ('title',)}
+    formfield_overrides = {
+        models.IntegerField: {
+        'widget': NumberInput(attrs={'size': '10'})
+        },
+    }
+    show_facets =admin.ShowFacets.ALWAYS
+admin.site.register(Listing, ListingAdmin)
