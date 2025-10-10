f97a906dfbf075299a77795af1fe6c1382d9978c
tim <count_francis@hotmail.com>
2025-10-10 16:03:50 +0800

    Step 15 : search done

--- listings/models.py
+++ listings/models.py
@@ -1,22 +1,28 @@
 from django.db import models
 from datetime import datetime
 from doctors.models import Doctor
-from . choices import district_choices
+from . choices import district_choices,room_choices, rooms_choices
+from taggit.managers import TaggableManager
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
 class Listing(models.Model):
     doctor = models.ForeignKey(Doctor ,on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     district = models.CharField(max_length =50, choices=district_choices.items())
-    services = models.CharField(max_length=200)
-    service = models.IntegerField()
-    screens = models.CharField(max_length=200)
-    screen = models.IntegerField()
-    professionals = models.CharField(max_length=200)
-    professional = models.IntegerField()
-    rooms = models.IntegerField()
     description = models.TextField(blank=True)
+    services = TaggableManager(verbose_name="Services")
+    service = models.IntegerField()
+    room_type = models.CharField(max_length=200, choices=room_choices.items(), default='')
+    screen = models.IntegerField()
+    professionals = models.ManyToManyField(Subject, blank=True)
+    professional = models.IntegerField()
+    rooms = models.CharField(max_length=2, choices=rooms_choices.items())  
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
@@ -34,6 +40,8 @@ class Listing(models.Model):
     def __str__(self):
         return self.title
     
+    def tag_list(self):
+        return u", ".join(tag.name for tag in self.services.all())
 
     
 
--- pages/views.py
+++ pages/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 from listings.models import Listing
 from doctors.models import Doctor
-from listings.choices import district_choices, room_choices, night_choices
+from listings.choices import district_choices, room_choices, rooms_choices
 
 def index(request):
     listings = Listing.objects.filter(is_published=True)[:3]
     context = {"listings" : listings,
                "district_choices" : district_choices,
                "room_choices" : room_choices,
-               "night_choices" : night_choices
+               "rooms_choices" : rooms_choices
                }
     return render(request, 'pages/index.html', context) 
 
